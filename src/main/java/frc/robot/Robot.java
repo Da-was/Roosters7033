4fa538b4da3d976857b7b19d20fc1bc85c9833ae
joao laurindo <joaoviniciuslaurindo@live.com>
2022-07-19 10:52:28 -0300

    mensagem legal

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right); 
 
 /*
- * joao é muito gostoso
+ * joao é muito gostoso e eu gosto de manga
  */
    
 
