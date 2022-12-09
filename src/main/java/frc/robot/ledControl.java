50cc87d6cc007c099581abc3344cf2d41b21519b
anannunciato <anac.annunciato18@gmail.com>
2022-12-09 17:12:18 -0300

    Led está funcionando.

--- src/main/java/frc/robot/ledControl.java
+++ src/main/java/frc/robot/ledControl.java
@@ -22,40 +22,49 @@ public class ledControl implements Runnable {
        return arrayPinos;
    }
 
+   /**
+    * Os parametros precisam ser em ordem Vermelho, Verde e Azul.
+    * @param Red
+    * @param Green
+    * @param Blue
+    */
    public ledControl(int Red, int Green, int Blue){
         this.Red = Red;
         this.Green = Green;
         this.Blue = Blue;
 
         this.redDI   = new DigitalOutput(this.Red);
-        this.greenDI = new DigitalOutput(this.Blue);
-        this.BlueDI  = new DigitalOutput(this.Green);
+        this.greenDI = new DigitalOutput(this.Green);
+        this.BlueDI  = new DigitalOutput(this.Blue);
+
+        this.settedPin = this.Red;
+        this.timer.start();
    }
 
-   public void run(){
-       System.out.println("running");
-    int pin =  this.settedPin;
-    if(pin == 0 || pin == this.Blue){
-        pin = this.Red;
-    }
-    timer.start();
-    if(timer.get() == delay){
+   public void run(){    
+    
+    if(this.timer.get() >= delay){
+        if(this.settedPin < this.Red  || this.settedPin > this.Blue){
+            this.settedPin = this.Red;
+        }
 
-         if(this.Red == pin){
+        int pin =  this.settedPin;
+        
+         if(pin == this.Red){//7
             this.redDI.set(true);
             System.out.println("vermelho on");
             this.greenDI.set(false);
             System.out.println("verde off");
             this.BlueDI.set(false);
             System.out.println("azul off");
-         }else if(this.Green == pin){
+         }else if(pin == this.Green){//8
             this.redDI.set(false);
             System.out.println("vermelho off");
             this.greenDI.set(true);
             System.out.println("verde on");
             this.BlueDI.set(false);
             System.out.println("azul off");
-         }else if(this.Blue == pin){
+         }else if(pin == this.Blue){//9
             this.redDI.set(false);
             System.out.println("vermelho off");
             this.greenDI.set(false);
@@ -64,9 +73,8 @@ public class ledControl implements Runnable {
             System.out.println("azul on");
          }
         
+         this.settedPin++;
+         this.timer.reset();
     }
-    this.settedPin++;
-
-    timer.reset();
    }
 }
